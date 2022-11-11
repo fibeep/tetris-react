0f5ad43749979a3912ec8e645679fb9db2185884
Salo <salomonco@gmail.com>
2022-11-11 16:32:34 -0500

    game over implemented

--- tetris/src/components/Controls.js
+++ tetris/src/components/Controls.js
@@ -5,48 +5,65 @@ import { moveDown, moveLeft, moveRight, rotate } from "../actions";
 
 export default function Controls(props) {
 	const dispatch = useDispatch();
-  	const isRunning = useSelector((state) => state.isRunning);
+  	const isRunning = useSelector((state) => state.game.isRunning);
+    const gameOver = useSelector((state) => state.game.gameOver); 
+
+
 	return (
-    <div className="controls">
+    <div className={`controls`}>
       {/* left */}
       <button
+        disabled={!isRunning || gameOver}
         className="control-button"
         onClick={(e) => {
+          if (!isRunning || gameOver) {
+            return;
+          }
           dispatch(moveLeft());
         }}
       >
         Left
       </button>
-
       {/* right */}
       <button
+        disabled={!isRunning || gameOver}
         className="control-button"
         onClick={(e) => {
+          if (!isRunning || gameOver) {
+            return;
+          }
           dispatch(moveRight());
         }}
       >
         Right
       </button>
-
       {/* rotate */}
       <button
+        disabled={!isRunning || gameOver}
         className="control-button"
         onClick={(e) => {
+          if (!isRunning || gameOver) {
+            return;
+          }
           dispatch(rotate());
         }}
       >
         Rotate
       </button>
-
       {/* down */}
       <button
+        disabled={!isRunning || gameOver}
         className="control-button"
         onClick={(e) => {
+          if (!isRunning || gameOver) {
+            return;
+          }
           dispatch(moveDown());
         }}
       >
         Down
       </button>
+      
     </div>
   );
 }
